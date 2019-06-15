27eeba50bf9029c3c4c098f087f4c048d2e2a67a
PRAKASH4150 <51699368+PRAKASH4150@users.noreply.github.com>
2019-06-15 12:07:49 +0530

    Create fibonacii.c

---
+++ fibonacii.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,res[50],arr[50];
+	printf("\n Enter the greatest number up to which you want to find out the fibonacii series for:");
+	scanf("%d",&n);
+	//res[50]=res[n];
+	for(i=0;i<n;i++)
+	{
+		arr[i]=i+1;
+	}
+	res[0]=1;
+	res[1]=2;
+	printf("%d %d ",res[0],res[1]);
+    for(i=2;i<n;i++)
+       {
+		res[i]=res[i-1]+res[i-2];
+        printf("%d ",res[i]);   
+}
+	return(0);
+}
