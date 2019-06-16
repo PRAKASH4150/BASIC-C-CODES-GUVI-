53c4ca8b0ebf234977b8f5b81759e53d236024db
PRAKASH4150 <51699368+PRAKASH4150@users.noreply.github.com>
2019-06-16 19:27:44 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,revnumber=0,temp,res;
+    scanf("%d",&n);
+    res=n;
+    if(n<=1000)
+    {
+    while(n!=0)
+    {
+        temp=n%10;
+        revnumber=revnumber*10+temp;
+        n=n/10;
+    }
+    if(revnumber==res)
+    {
+        printf("yes");
+    }
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
