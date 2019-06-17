dccd3e6e9813fc7ec6907893b1a6c3fcb742c3bd
Prakash Naidu <51699368+PRAKASH4150@users.noreply.github.com>
2019-06-17 15:10:58 +0530

    Create C program to print the greatest of two strings.c

---
+++ C program to print the greatest of two strings.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+     char str1[20],str2[20];
+     int l1,l2;
+     scanf("%s %s",&str1,&str2);
+     l1=strlen(str1);
+     l2=strlen(str2);
+     if(l1>l2)
+     {
+         puts(str1);
+     }
+     else if(l2>l1)
+     {
+         puts(str2);
+     }
+     else if(l1==l2)
+     {
+        puts(str1); 
+     }
+    
+    return 0;
+}
