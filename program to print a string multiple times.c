9fb7f9bf8595012129625773efc3d198e783f66f
Prakash Naidu <51699368+PRAKASH4150@users.noreply.github.com>
2019-06-17 14:44:32 +0530

    Create program to print a string multiple times.c

---
+++ program to print a string multiple times.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+    char ch[50];
+   // gets(ch);
+    scanf("\n %s %d",&ch,&n);
+    for(i=0;i<n;i++)
+    {
+         puts(ch);
+    }
+    
+    return 0;
+}
