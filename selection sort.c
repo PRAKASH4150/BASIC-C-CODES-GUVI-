6a6b75209cb9e02c90a8b55f331d15f2bfc75796
Prakash Naidu <51699368+PRAKASH4150@users.noreply.github.com>
2019-06-18 14:49:54 +0530

    Create selection sort.c

---
+++ selection sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[50],i,minpos,temp,n;
+    printf("\nENTER THE SIZE OF THE ARRAY:");
+    scanf("%d",&n);
+    printf("\nENTER THE ELEMENTS IN THE ARRAY:");
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+         if(arr[i]>arr[j])
+         { 
+             temp=arr[i];
+             arr[i]=arr[j];
+             arr[j]=temp;
+         }
+        }
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            printf("%d ",arr[i]);
+        }
+
+
+    return 0;
+}
