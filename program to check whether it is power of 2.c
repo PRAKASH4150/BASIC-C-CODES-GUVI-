ccd6bae78cae230d21873b38656026fd19842715
Prakash Naidu <51699368+PRAKASH4150@users.noreply.github.com>
2019-06-18 20:25:16 +0530

    Update program to check whether it is power of 2.c

--- program to check whether it is power of 2.c
+++ program to check whether it is power of 2.c
@@ -3,8 +3,15 @@ int main()
 {
     int n;
     scanf("%d",&n);
+    if(n==1)
+    {
+     printf("yes");    
+    }
+    else
+    {
     if(n%2==0)
     printf("yes");
     else
     printf("no");
+    }
 }
